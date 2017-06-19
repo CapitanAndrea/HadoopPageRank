1622ebdc5048c93213a1b4c8d9ed907cc649a141
CapitanAndrea <andrea.michienzi@libero.it>
2017-06-19 23:55:50 +0200

    Added passable arguments

--- src/main/java/it/pad/PageRankDriver.java
+++ src/main/java/it/pad/PageRankDriver.java
@@ -1,17 +1,18 @@
 package it.pad;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.NullWritable;
-import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-
-import java.util.TreeSet;
+import org.apache.hadoop.mapreduce.Job;
 
 import it.pad.parser.AdjacencyListMapper;
+import it.pad.parser.EdgeListMapper;
 import it.pad.parser.ParserReducer;
 import it.pad.pageranker.RankerMapper;
 import it.pad.pageranker.RankerReducer;
@@ -19,26 +20,97 @@ import it.pad.sorter.SorterMapper;
 import it.pad.sorter.DescendingRankSorterReducer;
 import it.pad.PageRankConstants;
 
-//TODO: vedrai mi tocca far specificare come argomento il numero di nodi del grafo! è ragionevole, ma bleah =\
+//TODO: aggiungere test
+//TODO: ricontrollare codice
+//TODO: pulire codice
 
-public class PageRankDriver{
+/**
+	* usage: -p parserClassName -s sorterClassName -n numberOfNodes -i inputFile -o outputFile [-m maxIterations] [-e errorThreshold] [-d dampingFactor] [-r numReducers]
+	*/
+public class PageRankDriver extends Configured implements Tool{
 
-	public static void main(String[] args) throws Exception{
+	public int run(String[] args) throws Exception{
+	/*	necessary arguments	*/
+		long nodes=-1;
+		Class parserClass=null;
+		Class sorterClass=null;
+		String inputFile=null;
+		String outputFile=null;
+	/*	optional arguments with default values	*/
 		float dampingFactor=0.85f;
-		double error=0;
-		int iterations=0;
+		double errorThreshold=0;
 		int maxIterations=10;
-		long nodes=0;
+		int numReducers=1;
+	/*	computation variables	*/		
+		int iterations=0;
+		double error=0;
 		int result_nodes=0;
 		
 		/*	arguments parsing	*/
-		
+		if(args.length%2!=0) return -1;
+		for(int i=0; i<args.length; i++){
+			//	parser class
+			if(args[i].compareTo("-p")==0){
+				i++;
+				parserClass=Class.forName(args[i]);
+				continue;
+			}
+			//	sorter class
+			if(args[i].compareTo("-s")==0){
+				i++;
+				sorterClass=Class.forName(args[i]);
+				continue;
+			}
+			//	number of nodes in the input graph
+			if(args[i].compareTo("-n")==0){
+				i++;
+				nodes=Long.parseLong(args[i]);
+				continue;
+			}
+			//	path of the input file
+			if(args[i].compareTo("-i")==0){
+				i++;
+				inputFile=args[i];
+				continue;
+			}
+			//	path of the output file
+			if(args[i].compareTo("-o")==0){
+				i++;
+				outputFile=args[i];
+				continue;
+			}
+			//	max number of iterations of the approximation
+			if(args[i].compareTo("-m")==0){
+				i++;
+				maxIterations=Integer.parseInt(args[i]);
+				continue;
+			}
+			//	error threshold to reach before stopping
+			if(args[i].compareTo("-e")==0){
+				i++;
+				errorThreshold=Double.parseDouble(args[i]);
+				continue;
+			}
+			//	damping factor to be used
+			if(args[i].compareTo("-d")==0){
+				i++;
+				dampingFactor=Float.parseFloat(args[i]);
+				continue;
+			}
+			//	number of reducers to be used
+			if(args[i].compareTo("-r")==0){
+				i++;
+				numReducers=Integer.parseInt(args[i]);
+				continue;
+			}
+		}
+		if(nodes<0 || parserClass==null || sorterClass==null || inputFile==null || outputFile==null) return -1;
+	
 		/*	parsing job	*/
 		Job parsingJob=new Job(new Configuration(), "parsing_job");
 		parsingJob.setJarByClass(PageRankDriver.class);
 
-//	TODO: aggiungere la possiblità di scegliere il parser?
-		parsingJob.setMapperClass(AdjacencyListMapper.class);
+		parsingJob.setMapperClass(parserClass);
 		parsingJob.setReducerClass(ParserReducer.class);
 		
 		parsingJob.setMapOutputKeyClass(Text.class);
@@ -47,15 +119,14 @@ public class PageRankDriver{
 		parsingJob.setOutputKeyClass(NullWritable.class);
 		parsingJob.setOutputValueClass(PageRankWritable.class);
 		
-		FileInputFormat.setInputPaths(parsingJob, new Path(args[0]));
+		FileInputFormat.setInputPaths(parsingJob, new Path(inputFile));
 		FileOutputFormat.setOutputPath(parsingJob, new Path("pr_0"));
 		
-		//parsingJob.getConfiguration().setFloat(PageRankConstants.DF_KEY, dampingFactor);
+		parsingJob.getConfiguration().setFloat(PageRankConstants.DF_KEY, dampingFactor);
 		parsingJob.waitForCompletion(true);
 		nodes=parsingJob.getCounters().findCounter(PageRankCounters.NODES_COUNT).getValue();
 		
 		/*	page rank computation	*/
-		
 		Configuration configuration=new Configuration();
 		configuration.setFloat(PageRankConstants.DF_KEY, dampingFactor);
 		configuration.setLong(PageRankConstants.N_KEY, nodes);
@@ -88,13 +159,11 @@ public class PageRankDriver{
 		}
 		
 		/*	final sort	*/
-		
 		Job sortingJob=new Job(new Configuration(), "sorting_job");
 		sortingJob.setJarByClass(PageRankDriver.class);
 		
 		sortingJob.setMapperClass(SorterMapper.class);
-//	TODO: aggiungere la possibilità di scegliere il modo di sortare gli elementi?
-		sortingJob.setReducerClass(DescendingRankSorterReducer.class);
+		sortingJob.setReducerClass(sorterClass);
 		
 		sortingJob.setMapOutputKeyClass(NullWritable.class);
 		sortingJob.setMapOutputValueClass(PageRankWritable.class);
@@ -106,11 +175,15 @@ public class PageRankDriver{
 		
 		Path input=new Path("pr_" + (iterations-1));
 		FileInputFormat.setInputPaths(sortingJob, input);
-		FileOutputFormat.setOutputPath(sortingJob, new Path("result"));
+		FileOutputFormat.setOutputPath(sortingJob, new Path(outputFile));
 		
 		sortingJob.waitForCompletion(true);
 		
 		fs.delete(input, true);
+		return 0;
+	}
 
+	public static void main(String[] args) throws Exception{
+		int result=new PageRankDriver().run(args);
 	}
 }
